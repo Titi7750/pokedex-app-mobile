a069062ba9e13092c21f6d884324f992e7a96444
Titi <tristanfio05@gmail.com>
2024-03-28 14:00:26 +0100

    Add Maps with MapView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faMapLocation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StrictMode } from "react";
 import { TouchableOpacity } from "react-native";
 import Home from "./components/home";
-import Profile from "./components/profile";
+import MapsPokemon from "./components/maps";
 
 function MyStack() {
   return (
@@ -16,16 +16,17 @@ function MyStack() {
         options={({ navigation }) => ({
           title: "Bienvenue sur PokeBuild",
           headerRight: () => (
-            <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
-              <FontAwesomeIcon icon={faUserCircle} size={30} color={"orange"} />
+            <TouchableOpacity
+              onPress={() => navigation.navigate("MapsPokemon")}
+            >
+              <FontAwesomeIcon icon={faMapLocation} size={30} color={"orange"} />
             </TouchableOpacity>
           ),
         })}
       />
       <Stack.Screen
-        name="Profile"
-        component={Profile}
-        options={{ title: "Profil" }}
+        name="MapsPokemon"
+        component={MapsPokemon}
       />
     </Stack.Navigator>
   );
